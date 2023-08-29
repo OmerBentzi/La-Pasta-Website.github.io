e048cafbeddb646700f762f4eaa2f3856dbe4ee1
yuval9757 <142411070+yuval9757@users.noreply.github.com>
2023-08-29 19:51:53 +0000

    sort error icon

--- public/form_sucsess.js
+++ public/form_sucsess.js
@@ -1,2 +1,2 @@
 var name = localStorage.getItem("sign_up_user_name");
-document.getElementById("Text").innerHTML = "Hi , " + name + " Thank you for signing up! We're excited to have you join us."
+document.getElementById("Text").innerHTML = "Hi , " + name + " Thank you for signing up! We're excited to have you join us.";
